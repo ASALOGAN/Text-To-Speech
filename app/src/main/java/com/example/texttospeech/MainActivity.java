6f6b2dcef8d9df2365214ab2483d63cd42b00a3e
ASALOGAN <AHMEDSHOIEBAQHTAR@GMAIL.COM>
2021-08-24 11:28:50 +0530

    Text To Speech

---
+++ app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.texttospeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Text;
+    Button BTN;
+    TextToSpeech textToSpeech;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Text = findViewById(R.id.Text);
+        BTN = findViewById(R.id.BTN);
+
+        // create an object textToSpeech and adding features into it
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                // if No error is found then only it will run
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        // Adding OnClickListener
+        BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(Text.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+    }
+}
